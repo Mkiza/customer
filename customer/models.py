0be6773649c2f8b8ec1eec352f21dacf9589f0ce
Mkiza <ronaldbagenda@gmail.com>
2020-08-04 20:34:27 +0200

    Some comments

--- customer/models.py
+++ customer/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
+    #Character fields to store string
     name = models.CharField(max_length=60)
     email = models.CharField(max_length=60)
     phone = models.CharField(max_length=60)
+    # __str__ method tells Djnago what to print when it needs to print out an instance of customer object
     def __str__(self):
         return self.name
